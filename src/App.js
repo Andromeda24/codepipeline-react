bc8a54fed8acdd32591493b07c59c9aeaa9b8dbe
Luisa Lamprea <llamprea@hotmail.com>
2025-01-28 21:31:03 -0600

    This is version 2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World. Welcome to my application</h1>
+      <h1>Hello World. Welcome to my application. This is version 2</h1>
     </div>
   );
 }
